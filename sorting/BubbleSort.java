face073f43e32f6a43db951c9aae6ef2e0429c45
Anas Raza <anasr12003@gmail.com>
2026-08-13 23:49:12 +0530

    update

--- sorting/BubbleSort.java
+++ sorting/BubbleSort.java
@@ -2,7 +2,7 @@ package sorting;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] arr = { 7, 8, 4, 9, 1 };
+        int[] arr = { 7, 8, 4, 9, 1 ,0};
         //time complexity O(n^2)
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - i - 1; j++) {
