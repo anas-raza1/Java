984400d460d288758725b461ad150151713b13e2
Anas Raza <anasr12003@gmail.com>
2026-08-18 23:47:50 +0530

    update

--- recursion/RemoveDuplicateString.java
+++ recursion/RemoveDuplicateString.java
@@ -1,4 +1,21 @@
 public class RemoveDuplicateString{
+    public static boolean[] map = new boolean[26];
+
+    public static void removeDuplicate(String str, int idx, String newString){
+        if(idx == str.length()){
+            System.out.print(newString);
+            return;
+        }
+        char currChar = str.charAt(idx);
+        if(map[currChar - 'a']){
+            removeDuplicate(str, idx+1, newString);
+        } else {
+            newString += currChar;
+            map[currChar - 'a'] = true;
+            removeDuplicate(str, idx+1, newString);
+        }
+    }
+
     static String remDuplicate(String str){
         if(str.length()<=1){
             return str;
@@ -12,5 +29,7 @@ public class RemoveDuplicateString{
     public static void main(String[] args) {
         String str = "aaaavvvddsvvdss";
         System.out.println(remDuplicate(str));
+
+        removeDuplicate(str, 0, "");
     }
 }
